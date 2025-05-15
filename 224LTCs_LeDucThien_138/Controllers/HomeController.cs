ba11ebce3c7ff8a605a9ced630c950ab0fbfdd89
ThienLeDuc <anhchangnhonhan020904@gmail.com>
2025-05-15 19:07:14 +0700

    Sao chép từ .../admin/home/scrud

---
+++ 224LTCs_LeDucThien_138/Controllers/HomeController.cs
@@ -0,0 +1,121 @@
+﻿using _224LTCs_LeDucThien_138.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _224LTCs_LeDucThien_138.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ConnectionDatabase _connectionDatabase;
+        private readonly PhongHocRepos _phongHocRepos;
+
+        public HomeController(ConnectionDatabase connectionDatabase)
+        {
+            _connectionDatabase = connectionDatabase;
+            _phongHocRepos = new PhongHocRepos(_connectionDatabase);
+        }
+
+        public IActionResult Index()
+        {
+            var phong = _phongHocRepos.GetAllPhong();
+            if (phong == null) {
+                return RedirectToAction("Error401", "Error");
+            }
+            return View(phong);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ThemPhong(PhongHoc phong)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!_phongHocRepos.IsTenPhongExists(phong.TenPhong)) {
+
+                    bool isAdded = _phongHocRepos.AddPhong(phong);
+
+                    if (isAdded)
+                    {
+                        TempData["SuccessMessage"] = "Đã được thêm thành công!";
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        TempData["ErrorMessage"] = "Có lỗi xảy ra khi thêm";
+                    }
+                } else
+                {
+                    TempData["ErrorMessage"] = "Lỗi: Tên phòng đã tồn tại";
+                }
+            }
+            return Redirect("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SuaPhong(PhongHoc phong)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!_phongHocRepos.IsTenPhongExists(phong.TenPhong) || _phongHocRepos.IsThisPhong(phong.MaPhong, phong.TenPhong))   
+                {
+
+                    bool isAdded = _phongHocRepos.UpdatePhong(phong);
+
+                    if (isAdded)
+                    {
+                        TempData["SuccessMessage"] = "Đã được cập nhật thành công!";
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        TempData["ErrorMessage"] = "Có lỗi xảy ra khi cập nhật";
+                    }
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Lỗi: Tên phòng đã tồn tại";
+                }
+            }
+            return Redirect("Index");
+        }
+
+
+        public IActionResult XoaPhong(int maPhong)
+        {
+            bool isDeleted = _phongHocRepos.DeletePhong(maPhong);
+
+            if (isDeleted)
+            {
+                TempData["SuccessMessage"] = "Xóa thành công!";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Không tìm thấy hoặc có lỗi khi xóa!";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult TimKiem(string TuKhoa)
+        {
+            List<PhongHoc> phong;
+            if (string.IsNullOrWhiteSpace(TuKhoa))
+            {
+                phong = _phongHocRepos.GetAllPhong();
+            }
+            else
+            {
+                phong = _phongHocRepos.TimKiemPhong(TuKhoa.Trim());
+            }
+
+            if (phong == null || !phong.Any())
+            {
+                TempData["Message"] = "Không tìm thấy kết quả nào.";
+            }
+
+            return View(phong);
+        }
+    
+    }
+}
---
+++ 224LTCs_LeDucThien_138/Models/ConnectionDatabase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+namespace _224LTCs_LeDucThien_138.Models
+{
+    public class ConnectionDatabase
+    {
+        private readonly string _databaseName;
+
+        public ConnectionDatabase(IConfiguration configuration)
+        {
+            _databaseName = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public SqlConnection GetConnection()
+        {
+            try
+            {
+                // Tạo và trả về đối tượng SqlConnection
+                SqlConnection connection = new SqlConnection(_databaseName);
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi và có thể ném lỗi hoặc trả về null
+                Console.WriteLine("Lỗi khi tạo kết nối: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
