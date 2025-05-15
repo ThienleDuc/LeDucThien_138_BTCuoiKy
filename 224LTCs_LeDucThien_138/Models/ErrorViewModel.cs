f6aef32723714b418eecb194adfebe14772d1be5
ThienLeDuc <anhchangnhonhan020904@gmail.com>
2025-05-15 20:50:48 +0700

    Sao chép file từ nhánh .../admin/canbo/scrud

---
+++ 224LTCs_LeDucThien_138/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _224LTCs_LeDucThien_138.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
